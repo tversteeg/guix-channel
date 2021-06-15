14a28c83ccaf4a78195bebf36d342b897ab3fea5
Thomas Versteeg <t@versteeg.email>
2021-06-15 22:03:18 +0200

    Add partial neovim nightly installation

--- local/neovim.scm
+++ local/neovim.scm
@@ -4,7 +4,18 @@
 	       #:use-module (guix utils)
 	       #:use-module (guix download)
 	       #:use-module (guix git-download)
-	       #:use-module (guix build-system copy))
+	       #:use-module (guix build-system cmake)
+	       #:use-module (guix build-system copy)
+	       #:use-module (gnu packages base)
+	       #:use-module (gnu packages gettext)
+	       #:use-module (gnu packages gperf)
+	       #:use-module (gnu packages jemalloc)
+	       #:use-module (gnu packages pkg-config)
+	       #:use-module (gnu packages libevent)
+	       #:use-module (gnu packages lua)
+	       #:use-module (gnu packages serialization)
+	       #:use-module (gnu packages terminals)
+	       )
 
 ;; There are no release tarballs.
 (define-public vim-rust
@@ -98,3 +109,90 @@
 		 (synopsis "Toggles between hybrid and absolute line numbers automatically in Neovim")
 		 (description
 		   "Numbertoggle is a Neovim plugin that in a buffer with 'hybrid' line numbers (:set number relativenumber), numbertoggle switches to absolute line numbers (:set number norelativenumber) automatically when relative numbers don't make sense.")))
+
+(define-public neovim-nightly
+	       (let ((commit "6a77def1ee05d4e4eceddb559bc779cd9b805614")
+		     (revision "0"))
+		 (package
+		   (name "neovim-nightly")
+		   (version (string-append "0.5.0-" revision "." (string-take commit 7)))
+		   (source
+		     (origin
+		       (method git-fetch)
+		       (uri (git-reference
+			      (url "https://github.com/neovim/neovim")
+			      (commit commit)))
+		       (file-name (string-append name "-" version "-checkout"))
+		       (sha256
+			 (base32 "13029a60bip8nmlny4p6hrvvmhmw8d4cdl9waacdbbh9cmxlchyn"))))
+		   (build-system cmake-build-system)
+		   (arguments
+		     `(#:modules ((srfi srfi-26)
+				  (guix build cmake-build-system)
+				  (guix build utils))
+		       #:configure-flags '("-DPREFER_LUA:BOOL=YES")
+		       #:phases
+		       (modify-phases %standard-phases
+				      (add-after 'unpack 'set-lua-paths
+						 (lambda* (#:key inputs #:allow-other-keys)
+							  (let* ((lua-version "5.1")
+								 (lua-cpath-spec
+								   (lambda (prefix)
+								     (let ((path (string-append prefix "/lib/lua/" lua-version)))
+								       (string-append path "/?.so;" path "/?/?.so"))))
+								 (lua-path-spec
+								   (lambda (prefix)
+								     (let ((path (string-append prefix "/share/lua/" lua-version)))
+								       (string-append path "/?.lua;" path "/?/?.lua"))))
+								 (lua-inputs (map (cute assoc-ref %build-inputs <>)
+										  '("lua"
+										    "lua-luv"
+										    "lua-lpeg"
+										    "lua-bitop"
+										    "lua-libmpack"))))
+							    (setenv "LUA_PATH"
+								    (string-join (map lua-path-spec lua-inputs) ";"))
+							    (setenv "LUA_CPATH"
+								    (string-join (map lua-cpath-spec lua-inputs) ";"))
+							    #t)))
+				      (add-after 'unpack 'prevent-embedding-gcc-store-path
+						 (lambda _
+						   ;; nvim remembers its build options, including the compiler with
+						   ;; its complete path.  This adds gcc to the closure of nvim, which
+						   ;; doubles its size.  We remove the refirence here.
+						   (substitute* "cmake/GetCompileFlags.cmake"
+								(("\\$\\{CMAKE_C_COMPILER\\}") "/gnu/store/.../bin/gcc"))
+						   #t)))))
+		   (inputs
+		     `(("libuv" ,libuv)
+		       ("msgpack" ,msgpack)
+		       ("libtermkey" ,libtermkey)
+		       ("libvterm" ,libvterm)
+		       ("unibilium" ,unibilium)
+		       ("jemalloc" ,jemalloc)
+		       ("libiconv" ,libiconv)
+		       ("lua" ,lua-5.1)
+		       ("lua-luv" ,lua5.1-luv)
+		       ("lua-lpeg" ,lua5.1-lpeg)
+		       ("lua-bitop" ,lua5.1-bitop)
+		       ("lua-libmpack" ,lua5.1-libmpack)))
+		   (native-inputs
+		     `(("pkg-config" ,pkg-config)
+		       ("gettext" ,gettext-minimal)
+		       ("gperf" ,gperf)))
+		   (home-page "https://neovim.io")
+		   (synopsis "Fork of vim focused on extensibility and agility")
+		   (description "Neovim is a project that seeks to aggressively
+				refactor Vim in order to:
+				@itemize
+				@item Simplify maintenance and encourage contributions
+				@item Split the work between multiple developers
+				@item Enable advanced external UIs without modifications to the core
+				@item Improve extensibility with a new plugin architecture
+				@end itemize\n")
+				;; Neovim is licensed under the terms of the Apache 2.0 license,
+				;; except for parts that were contributed under the Vim license.
+				(license (list license:asl2.0 license:vim))))
+		 )
+
+neovim-nightly
